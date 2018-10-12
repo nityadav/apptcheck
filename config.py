74fdb7520e4bb617a115463a06999d917e343de5
Nitin <nityadav@users.noreply.github.com>
2018-10-12 10:29:35 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 offices = {
-    'south bay': [('Santa Clara', '632'), ('San Jose', '516')],
-    'bay': [('San Francisco', '503'), ('Redwood City', '548')]
+    'south bay': [('FREMONT', '644'), ('GILROY', '623'), ('LOS GATOS', '640'), ('SAN JOSE', '516'), ('SAN JOSE DLPC', '645'), ('SANTA CLARA', '632'), ('SANTA TERESA', '668')],
+    'bay': [('DALY CITY', '599'), ('REDWOOD CITY', '548'), ('SAN FRANCISCO', '503'), ('SAN MATEO', '593')]
 }
